e299c6fd50cb1bde566f0fc261f0f4b9b0344d5d
Maximiliano Castrejon <mcastrejon.dev.2021@gmail.com>
2022-06-04 11:45:15 -0500

    first commit

---
+++ userCalendar/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UsercalendarConfig(AppConfig):
+    name = 'userCalendar'
---
+++ users_interactions/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UsersInteractionsConfig(AppConfig):
+    name = 'users_interactions'
